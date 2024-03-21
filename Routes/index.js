cf052007573db19d485335753a68a6f955b165fd
Abhinav Anand <abhinava32@gmail.com>
2024-03-21 22:06:53 +0530

    Added filter by department and search by id API's

--- Routes/index.js
+++ Routes/index.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const addEmployee = require('../Controller/AddEmployee');
+const FilterByDept = require('../Controller/FilterByDept');
+const Search = require('../Controller/Search');
+
 
 router.post('/add', addEmployee.addEmployee);
+router.get('/dept/:department', FilterByDept.FilterByDept);
+router.get('/search/:id', Search.Search);
 
 module.exports = router;
